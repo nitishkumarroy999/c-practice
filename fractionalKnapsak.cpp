aae8e76f9f4a488eb5bb8c18f709ba2a057a7e7c
nitishkumarroy999 <nitishkumarroy526@gmail.com>
2022-12-30 19:20:18 +0530

    fract

---
+++ fractionalKnapsak.cpp
@@ -0,0 +1,83 @@
+//fractional knapsack greedy technique
+//Nkr codes
+#include<iostream>
+#include <bitstring.h>
+using namespace std;
+typedef struct {
+   int v;
+   int w;
+   float d;
+} Item;
+void input(Item items[],int sizeOfItems) {
+   cout << "Enter total "<< sizeOfItems <<" item's values and weight" <<
+   endl;
+   for(int i = 0; i < sizeOfItems; i++) {
+      cout << "Enter "<< i+1 << " V ";
+      cin >> items[i].v;
+      cout << "Enter "<< i+1 << " W ";
+      cin >> items[i].w;
+   }
+}
+void display(Item items[], int sizeOfItems) {
+   int i;
+   cout << "values: ";
+   for(i = 0; i < sizeOfItems; i++) {
+      cout << items[i].v << "\t";
+   }
+   cout << endl << "weight: ";
+   for (i = 0; i < sizeOfItems; i++) {
+      cout << items[i].w << "\t";
+   }
+   cout << endl;
+}
+bool compare(Item i1, Item i2) {
+   return (i1.d > i2.d);
+}
+float knapsack(Item items[], int sizeOfItems, int W) {
+   int i, j;
+   float totalValue = 0, totalWeight = 0;
+   for (i = 0; i < sizeOfItems; i++) {
+      items[i].d = (float)items[i].v / items[i].w; //typecasting done (v is int and w is also int so we get final value of d as int)
+   }
+   sort(items, items+sizeOfItems, compare);
+   /*
+   uncomment if u need to check the data after sortingis done
+   cout << "values : ";
+   for(i = 0; i < sizeOfItems; i++) {
+      cout << items[i].v << "\t";
+   }
+   cout << endl << "weights: ";
+   for (i = 0; i < sizeOfItems; i++) {
+      cout << items[i].w << "\t";
+   }
+   cout << endl << "ratio  : ";
+   for (i = 0; i < sizeOfItems; i++) {
+      cout << items[i].d << "\t";
+   }
+   cout << endl;
+   */
+   for(i=0; i<sizeOfItems; i++) {
+      if(totalWeight + items[i].w<= W) {
+         totalValue += items[i].v ;
+         totalWeight += items[i].w;
+      } else {
+         int wt = W-totalWeight;
+         totalValue += (wt * items[i].d);
+         totalWeight += wt;
+         break;
+      }
+   }
+   cout << "Total weight in bag " << totalWeight<<endl;
+   return totalValue;
+}
+int main() {
+   int W;
+   Item items[4];
+   input(items, 4);
+   cout << "Entered data \n";
+   display(items,4);
+   cout<< "Enter Knapsack weight \n";
+   cin >> W;
+   float mxVal = knapsack(items, 4, W);
+   cout << "Max value for "<< W <<" weight is "<< mxVal;
+}
