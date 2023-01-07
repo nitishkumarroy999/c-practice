7a0901d24cb5783424ff5aad8273cbaeb6239343
nitishkumarroy999 <nitishkumarroy526@gmail.com>
2023-01-07 16:39:42 +0530

    ternary

--- ternary2.cpp
+++ ternary2.cpp
@@ -16,4 +16,4 @@ int main() {
   cout << "You " << result << " the exam.";
 
   return 0;
-}
+} 
---
+++ ternary4.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+//Nkr codes
+using namespace std;
+int main() {
+   int a = 10;
+   int b = 369;
+   int max = a > b ? a : b;
+   cout << "Maximum value = " << max << "\n";
+   return 0;
+}
