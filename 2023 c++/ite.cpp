e6d25da11d00e86fdb6cde45d1ea83bc92d49e64
nitishkumarroy999 <nitishkumarroy526@gmail.com>
2023-02-09 21:00:46 +0530

    n

---
+++ 2023 c++/ite.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+
+{
+
+int i, sum=0, n;
+
+cout<<"\n Enter The value of n";
+
+cin>>n;
+
+i =1;
+
+for ( ; i<=10;i++)
+
+      {
+
+      sum += i;
+
+     }
+
+cout<<"\n The sum of 1 to " <<n<<"is "<<sum;
+
+return 0;
+
+}
